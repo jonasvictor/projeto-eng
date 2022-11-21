fe0dc9668f5e3e9f2fadca673ffd17d6c6ad880a
ruan-pb <ruancruzsoares@hotmail.com>
2022-11-21 19:49:59 -0300

    resolvendo conflitos e adicionando validações para divisão com 0

--- projeto-git-eng/src/git/JonasRuan.java
+++ projeto-git-eng/src/git/JonasRuan.java
@@ -17,8 +17,27 @@ public static void main(String args[]) {
 		
 		resultado = numero1 + numero2;
 		
+<<<<<<< Updated upstream
 		JOptionPane.showMessageDialog(null, "O resultado da soma é: "+resultado);
+=======
+		resultadomult = numero1 * numero2;
 		
+		resultadosub = numero1 - numero2;
+		
+		resultadodiv = numero1 / numero2;
+		
+		JOptionPane.showMessageDialog(null, "O resultado da soma é: "+resultadosoma);
+		JOptionPane.showMessageDialog(null, "O resultado da multiplicação: "+resultadomult);
+>>>>>>> Stashed changes
+		
+		if(numero1 == 0 || numero2 == 0) {
+			JOptionPane.showMessageDialog(null, "Impossivel dividir um numero por 0:");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "O resultado da divisão: "+resultadodiv);
+		}
+		
+		JOptionPane.showMessageDialog(null, "O resultado da subtração: "+resultadosub);
 		
 
 		
